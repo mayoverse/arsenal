7a64ae6721af038bdacfb054a268b91689274b56
Ethan Heinzen <heinzen.ethan@mayo.edu>
2016-12-13 23:10:58 +0000

    Added %nin%
    git-svn-id: https://bsisvn.mayo.edu/main/projects/r/rpkg/arsenal/trunk@33180 8f329c14-d232-4f17-8684-7ee34322b8dc

---
+++ R/not.in.R
@@ -0,0 +1,26 @@
+###########################################################################################
+### Creation Date: 6/2015
+### Last Modified: Monday, 18 July 2016 03:00 PM CDT
+########################################################################################### 
+
+#' Not in
+#' 
+#' The not-in operator for R.
+#' 
+#' @inheritParams base::`%in%`
+#' @return The negation of \code{\link{\%nin\%}}.
+#' @examples
+#' 1 %nin% 2:10
+#' c("a", "b") %nin% c("a", "c", "d")
+#' @seealso \code{\link{\%in\%}}, \code{\link[Hmisc]{\%nin\%}}
+#' @author Raymond Moore
+#' @aliases nin
+#' @export
+
+## The not-in operator for R
+## From Raymond Moore, 6/2015, who found it on google
+## '%nin%' <- Negate('%in%')
+
+## sorry guys--Ethan changed this on 7/26/16
+
+`%nin%` <- function(x, table) match(x, table, nomatch = 0L) == 0L
