49bfb187765aabb8e9b5144e72e74e5b881d0bae
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-09-20 14:05:06 -0500

    Implement yaml functions

---
+++ R/yaml.R
@@ -0,0 +1,43 @@
+
+#' Include a YAML header in \code{write2}
+#'
+#' @param x An object of class \code{"yaml"}.
+#' @param ... For \code{yaml()}, arguments to be bundled into a list and passed to \code{\link[yaml]{as.yaml}}.
+#'   For \code{print.yaml()}, extra arguments. For \code{c.yaml()}, "yaml" objects to be concatenated.
+#' @param recursive Not in use at this time.
+#' @return A text string of class \code{"yaml"}.
+#' @author Ethan Heinzen, adapted from an idea by Brendan Broderick
+#' @name yaml
+NULL
+#> NULL
+
+#' @rdname yaml
+#' @export
+yaml <- function(...)
+{
+  x <- yaml::as.yaml(list(...))
+  class(x) <- "yaml"
+  x
+}
+
+#' @rdname yaml
+#' @export
+print.yaml <- function(x, ...)
+{
+  cat(c("---\n", x, "---\n"), sep = "")
+  invisible(x)
+}
+
+#' @rdname yaml
+#' @export
+c.yaml <- function(..., recursive = FALSE)
+{
+  structure(paste0(c(unlist(lapply(list(...), unclass))), collapse = ""), class = "yaml")
+}
+
+#' @rdname yaml
+#' @export
+is.yaml <- function(x)
+{
+  inherits(x, "yaml")
+}
