8efc03239a86e52a11a914cf66863165d3df4218
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-10-17 16:34:06 -0500

    incorporate exponentiated estimates

--- R/as.data.frame.modelsum.R
+++ R/as.data.frame.modelsum.R
@@ -1,5 +1,5 @@
 
-get_the_estimate <- function(fitList)
+get_the_estimate <- function(fitList, expon)
 {
   # even if the model doesn't have an intercept, that's okay
   labs <- c("(Intercept)", fitList$label, fitList$adjlabels)
@@ -10,23 +10,31 @@ get_the_estimate <- function(fitList)
     term = fitList$coeff$term,
     label = labs[fitList$coeff$term],
     term.type = ifelse(fitList$coeff$term == "(Intercept)", "Intercept", ifelse(fitList$coeff$term %in% fitList$adjterms, "Adjuster", "Term")),
-    estimate = fitList$coeff$estimate,
-    std.error = fitList$coeff$std.error,
-    p.value = fitList$coeff$p.value,
-    # even if the model doesn't have an adj.r.squared, that's okay.
     stringsAsFactors = FALSE
   )
+  if(expon && fitList$family %in% c("binomial", "quasibinomial", "survival", "poisson", "quasipoisson"))
+  {
+    type <- ifelse(fitList$family %in% c("binomial", "quasibinomial"), "OR", ifelse(fitList$family == "survival", "HR", "RR"))
+    out[[type]] <- fitList$coeff[[type]]
+    out[[paste0("CI.lower.", type)]] <- fitList$coeff[[paste0("CI.lower.", type)]]
+    out[[paste0("CI.upper.", type)]] <- fitList$coeff[[paste0("CI.upper.", type)]]
+  } else
+  {
+    out$estimate <- fitList$coeff$estimate
+    out$std.error <- fitList$coeff$std.error
+  }
+  out$p.value = fitList$coeff$p.value
   out$adj.r.squared <- fitList$glance$adj.r.squared
   out
 }
 
-as_data_frame.modelsum <- function(x, ..., labelTranslations = NULL, show.intercept = NA, show.adjust = NA)
+as_data_frame.modelsum <- function(x, ..., labelTranslations = NULL, show.intercept = NA, show.adjust = NA, exponentiate = FALSE)
 {
   if(!is.null(labelTranslations)) labels(x) <- labelTranslations
   if(is.na(show.intercept)) show.intercept <- x$control$show.intercept
   if(is.na(show.adjust)) show.adjust <- x$control$show.adjust
 
-  out <- do.call(rbind, Map(cbind, model = seq_along(x$fits), lapply(x$fits, get_the_estimate))) # this step is almost magic
+  out <- do.call(rbind, Map(cbind, model = seq_along(x$fits), lapply(x$fits, get_the_estimate, expon = exponentiate))) # this step is almost magic
   out <- out[out$term.type %in% c("Term", if(show.intercept) "Intercept", if(show.adjust) "Adjuster"), , drop = FALSE]
   row.names(out) <- NULL
   out
