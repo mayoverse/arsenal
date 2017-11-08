96a8e605a468c9f810d17d54a3d274a2dded7f1c
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-11-08 13:08:16 -0600

    Clean up modelsum.control a little bit

--- R/modelsum.control.R
+++ R/modelsum.control.R
@@ -36,7 +36,7 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
 	  warning("digits must be positive integer. Set to default. \n")
 	  digits <- 3
   }
-  if(conf.level <= 0 | conf.level >= 1) {
+  if(conf.level <= 0 || conf.level >= 1) {
     warning("conf.level must be between (0,1). Setting to default.\n")
     conf.level <- 0.95
   }
@@ -47,20 +47,22 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
   ##Other coefficient columns:
   ##CI.estimate, N, Nmiss2, depvar (show name of dependent variable), estimate, se, zstat
   ##Other  model fits:  logLik,AIC,BIC
-  binomial.stats.valid <- c(c("Nmiss","OR","CI.lower.OR","CI.upper.OR","p.value","concordance"), # default
-                 c("estimate","CI.OR","CI.estimate","CI.lower.estimate","CI.upper.estimate","N","Nmiss2","endpoint","std.error","statistic"),
-                 c("logLik","AIC","BIC","null.deviance","deviance","df.residual","df.null"))
+  binomial.stats.valid <- c(
+    "Nmiss", "OR", "CI.lower.OR", "CI.upper.OR", "p.value", "concordance",  # default
+    "estimate", "CI.OR", "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "N", "Nmiss2", "endpoint", "std.error", "statistic",
+    "logLik", "AIC", "BIC", "null.deviance", "deviance", "df.residual", "df.null"
+  )
 
-  if(any(!(binomial.stats %in% binomial.stats.valid))) {
+  if(any(binomial.stats %nin% binomial.stats.valid)) {
     stop(paste0("Invalid binomial stats: ",
-           paste(binomial.stats[!(binomial.stats %in% binomial.stats.valid)],collapse=","), "\n"))
+           paste(binomial.stats[binomial.stats %nin% binomial.stats.valid],collapse=","), "\n"))
   }
   ## let CI.OR decode to CI.lower.OR and CI.upper.OR
-  if(any(grepl("CI.OR", binomial.stats))) {
-    binomial.stats <- unique(c("CI.lower.OR","CI.upper.OR", binomial.stats[-grep("CI.OR",binomial.stats)]))
+  if(any(binomial.stats == "CI.OR")) {
+    binomial.stats <- unique(c("CI.lower.OR", "CI.upper.OR", binomial.stats[binomial.stats != "CI.OR"]))
   }
-  if(any(grepl("CI.estimate", binomial.stats))) {
-    binomial.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", binomial.stats[-grep("CI.estimate",binomial.stats)]))
+  if(any(binomial.stats == "CI.estimate")) {
+    binomial.stats <- unique(c("CI.lower.estimate", "CI.upper.estimate", binomial.stats[binomial.stats != "CI.estimate"]))
   }
 
   ##########################
@@ -68,40 +70,44 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
   ##########################
   ##Other coefficient columns: CI.estimate, N, Nmiss2, t.stat, standard.estimate, endpoint
   ##Other model fits: r.squared, AIC, BIC,logLik
-  gaussian.stats.valid <- c(c("Nmiss","estimate","std.error","p.value", "adj.r.squared"), #default
-              c("CI.estimate","CI.lower.estimate","CI.upper.estimate", "N", "Nmiss2", "statistic", "standard.estimate", "endpoint"),
-              c("r.squared", "AIC", "BIC","logLik","statistic.F","p.value.F"))
+  gaussian.stats.valid <- c(
+    "Nmiss", "estimate", "std.error", "p.value", "adj.r.squared",  #default
+    "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "N", "Nmiss2", "statistic", "standard.estimate", "endpoint",
+    "r.squared", "AIC", "BIC", "logLik", "statistic.F", "p.value.F"
+  )
 
-  if(any(!(gaussian.stats %in% gaussian.stats.valid))) {
+  if(any(gaussian.stats %nin% gaussian.stats.valid)) {
     stop(paste0("Invalid gaussian stats: ",
-           paste(gaussian.stats[!(gaussian.stats %in% gaussian.stats.valid)],collapse=","), "\n"))
+           paste(gaussian.stats[gaussian.stats %nin% gaussian.stats.valid],collapse=","), "\n"))
   }
-  if(any(grepl("CI.estimate", gaussian.stats))) {
-    gaussian.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", gaussian.stats[-grep("CI.estimate",gaussian.stats)]))
+  if(any(gaussian.stats == "CI.estimate")) {
+    gaussian.stats <- unique(c("CI.lower.estimate", "CI.upper.estimate", gaussian.stats[gaussian.stats != "CI.estimate"]))
   }
 
 
-##########################
+  ##########################
   ## Poisson stats:
   ##########################
   ##(quasi)/poisson.stats=c("Nmiss","RR","CI.RR", "p.value","concordance"),
   ##Other coeff columns: CI.estimate, CI.RR  (ci for relrisk),N,Nmiss2, std.error, estimate, z.stat, endpoint
   ##Other model fits: AIC,BIC,logLik, dispersion
   ##  dispersion = deviance/df.residual
-  poisson.stats.valid <- c(c("RR","CI.lower.RR","CI.upper.RR", "p.value","concordance", "Nmiss"), # default
-                   c("CI.RR","CI.estimate","CI.lower.estimate","CI.upper.estimate", "CI.RR", "Nmiss2", "std.error", "estimate", "statistic","endpoint"),
-                   c("AIC", "BIC","logLik","dispersion","null.deviance","deviance","df.residual","df.null"))
+  poisson.stats.valid <- c(
+    "RR", "CI.lower.RR", "CI.upper.RR", "p.value", "concordance", "Nmiss", # default
+    "CI.RR", "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "CI.RR", "Nmiss2", "std.error", "estimate", "statistic", "endpoint",
+    "AIC", "BIC", "logLik", "dispersion", "null.deviance", "deviance", "df.residual", "df.null"
+  )
 
-  if(any(!(poisson.stats %in% poisson.stats.valid))) {
+  if(any(poisson.stats %nin% poisson.stats.valid)) {
       stop(paste0("Invalid poisson stats: ",
-           paste(poisson.stats[!(poisson.stats %in% poisson.stats.valid)],collapse=","), "\n"))
+           paste(poisson.stats[poisson.stats %nin% poisson.stats.valid],collapse=","), "\n"))
   }
    ## let CI.RR decode to CI.lower.RR and CI.upper.RR
-  if(any(grepl("CI.RR", poisson.stats))) {
-    poisson.stats <- unique(c("CI.lower.RR","CI.upper.RR", poisson.stats[-grep("CI.RR",poisson.stats)]))
+  if(any(poisson.stats == "CI.RR")) {
+    poisson.stats <- unique(c("CI.lower.RR", "CI.upper.RR", poisson.stats[poisson.stats != "CI.RR"]))
   }
-  if(any(grepl("CI.estimate", poisson.stats))) {
-    poisson.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", poisson.stats[-grep("CI.estimate",poisson.stats)]))
+  if(any(poisson.stats == "CI.estimate")) {
+    poisson.stats <- unique(c("CI.lower.estimate", "CI.upper.estimate", poisson.stats[poisson.stats == "CI.estimate"]))
   }
   ##########################
   ## Survival stats:
@@ -109,20 +115,23 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
   ##surv.stats=c(Nmiss,HR,CI.HR,p.value,concorance)
   ##Other possible coefficient table columns: CI.estimate,N,Nmiss2,estimate,se,endpoint,Nevents,z.stat
   ##Other possible model fits: r.squared, logLik, AIC, BIC
-  surv.stats.valid <- c(c("HR","CI.lower.HR","CI.upper.HR","p.value","concordance","Nmiss"), # default
-            c("CI.HR","CI.estimate","CI.lower.estimate","CI.upper.estimate","N","Nmiss2","estimate","std.error","endpoint","Nevents","statistic"),
-            c("r.squared", "logLik", "AIC", "BIC","statistic.sc","p.value.sc","p.value.log","p.value.wald","N","std.error.concordance"))
-  if(any(!(survival.stats %in% surv.stats.valid))) {
+  surv.stats.valid <- c(
+    "HR", "CI.lower.HR", "CI.upper.HR", "p.value", "concordance", "Nmiss", # default
+    "CI.HR", "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "N", "Nmiss2", "estimate", "std.error", "endpoint", "Nevents", "statistic",
+    "r.squared", "logLik", "AIC", "BIC", "statistic.sc", "p.value.sc", "p.value.log", "p.value.wald", "N", "std.error.concordance"
+  )
+
+  if(any(survival.stats %nin% surv.stats.valid)) {
     stop(paste0("Invalid survival stats: ",
-           paste(survival.stats[!(survival.stats %in% surv.stats.valid)],collapse=","), "\n"))
+           paste(survival.stats[survival.stats %nin% surv.stats.valid], collapse=","), "\n"))
   }
 
   ## let CI.HR decode to CI.lower.HR and CI.upper.HR
-  if(any(grepl("CI.HR", survival.stats))) {
-    survival.stats <- unique(c("CI.lower.HR","CI.upper.HR", survival.stats[-grep("CI.HR",survival.stats)]))
+  if(any(survival.stats == "CI.HR")) {
+    survival.stats <- unique(c("CI.lower.HR", "CI.upper.HR", survival.stats[survival.stats != "CI.HR"]))
   }
-  if(any(grepl("CI.estimate", survival.stats))) {
-    survival.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", survival.stats[-grep("CI.estimate",survival.stats)]))
+  if(any(survival.stats == "CI.estimate")) {
+    survival.stats <- unique(c("CI.lower.estimate", "CI.upper.estimate", survival.stats[survival.stats != "CI.estimate"]))
   }
   return(list(digits=digits, digits.test=digits.test, nsmall=nsmall, nsmall.ratio=nsmall.ratio,
               show.adjust=show.adjust, show.intercept=show.intercept, conf.level=conf.level,
