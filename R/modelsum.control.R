548b764241139b5d576be4cdcf2652f838bd4de5
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-11-14 10:45:25 -0600

    Overhaul summary.modelsum (#40)
    * First pass at rewriting as.data.frame.modelsum
    
    * Add adjuster labels to modelsum object
    
    * Move adj.r.squared outside data.frame statement
    
    * incorporate exponentiated estimates
    
    * Avoid ifelse
    
    * Clean up modelsum.control a little bit
    
    * Change order of variables in modelsum.control
    
    * Total revamp of as_data_frame.modelsum, to include family.stats part of modelsum.control object
    
    * Tweak print.modelsum to remove for-loop and fix y-variable. Close #33.
    
    * Remove unneccesary function
    
    * Add control as data.frame attribute
    
    * Revamp digits and nsmall in modelsum.control
    
    * Remove "endpoint" from as_data_frame.modelsum. This can instead be specified by family.stats
    
    * Dang you, partial matching. Another fix for printing modelsum objects (#33).
    
    * Revamp [.modelsum, and add tests akin to those for tableby.
    
    * Add stat.labels to modelsum.control
    
    * First pass at summary.modelsum
    
    * Add Nmiss2 to modelGlance
    
    * Tidy up some modelsum code
    
    * Un-format integers, and eliminate Nmiss if no missing obs
    
    * Change formatting for summary_modelsum
    
    * Change some argument names in modelsum.control
    
    * Move the check for Nmiss to as_data_frame.modelsum
    
    * Rename as_data_frame.modelsum to as.data.frame.modelsum. Close #12. Close #31.
    
    * Rename summary_modelsum to summary.modelsum. Close #13. Close #37.
    
    * Update documentation
    
    * Fix reference to as_data_frame
    
    * Small tweaks to labels<-.modelsum
    
    * Add test for #13
    
    * Update modelsum tests
    
    * Update keep.labels test
    
    * Update write2 test

--- R/modelsum.control.R
+++ R/modelsum.control.R
@@ -6,40 +6,56 @@
 #'
 #' Control test and summary settings for \code{\link{modelsum}} function.
 #'
-#' @param digits Numeric, denoting the number of significant digits for beta coefficients and standard errors.
-#' @param digits.test Numeric, denoting the number of significant digits for p-values.
-#' @param nsmall Numeric, denoting the number of digits after the decimal point for beta coefficients and standard errors.
-#' @param nsmall.ratio Numeric, denoting the number of digits after the decimal point for ratios, e.g. OR, RR, HR.
+#' @param digits Numeric, denoting the number of digits after the decimal point for beta coefficients and standard errors.
+#' @param digits.ratio Numeric, denoting the number of digits after the decimal point for ratios, e.g. OR, RR, HR.
+#' @param digits.p Numeric, denoting the number of digits for p-values. See "Details", below.
+#' @param format.p Logical, denoting whether to format p-values. See "Details", below.
 #' @param show.adjust Logical, denoting whether to show adjustment terms.
 #' @param show.intercept Logical, denoting whether to show intercept terms.
 #' @param conf.level Numeric, giving the confidence level.
 #' @param binomial.stats,survival.stats,gaussian.stats,poisson.stats
 #'   Character vectors denoting which stats to show for the various model types.
+#' @param stat.labels A named list of labels for all the stats used above.
 #' @param ... Other arguments (not in use at this time).
 #' @return A list with settings to be used within the \code{modelsum} function.
+#' @details
+#'   If \code{format.p} is \code{FALSE}, \code{digits.p} denotes the number of significant digits shown. The
+#'   p-values will be in exponential notation if necessary. If \code{format.p} is \code{TRUE},
+#'   \code{digits.p} will determine the number of digits after the decimal point to show. If the p-value
+#'   is less than the resulting number of places, it will be formatted to show so.
 #' @seealso \code{\link{modelsum}}, \code{\link{summary.modelsum}}
 #' @export
-modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=3,
-            show.adjust=TRUE, show.intercept=TRUE, conf.level=0.95,
+modelsum.control <- function(digits = 3L, digits.ratio = 3L, digits.p = 3L, format.p = TRUE,
+            show.adjust = TRUE, show.intercept = TRUE, conf.level = 0.95,
             binomial.stats=c("OR","CI.lower.OR","CI.upper.OR","p.value", "concordance","Nmiss"),
             gaussian.stats=c("estimate","std.error","p.value","adj.r.squared","Nmiss"),
             poisson.stats=c("RR","CI.lower.RR", "CI.upper.RR","p.value","concordance","Nmiss"),
-            survival.stats=c("HR","CI.lower.HR","CI.upper.HR","p.value","concordance","Nmiss"),	...)
+            survival.stats=c("HR","CI.lower.HR","CI.upper.HR","p.value","concordance","Nmiss"),
+            stat.labels = list(), ...)
 {
 
-  ## validate digits
-  if(is.null(digits)) {
-    digits <- 3
+  # digits and digits.test are OK to be NULL. See ?format
+  if(!is.null(digits) && digits < 0L)
+  {
+    warning("digits must be >= 0. Set to default.")
+    digits <- 3L
+  }
+  if(!is.null(digits.ratio) && digits.ratio < 0L)
+  {
+    warning("digits.ratio must be >= 0. Set to default.")
+    digits.ratio <- 3L
+  }
+  if(!is.null(digits.p) && digits.p < 0L)
+  {
+    warning("digits.p must be >= 0. Set to default.")
+    digits.p <- 3L
   }
 
-  if(digits < 1) {
-	  warning("digits must be positive integer. Set to default. \n")
-	  digits <- 3
-  }
-  if(conf.level <= 0 | conf.level >= 1) {
+  if(conf.level <= 0 || conf.level >= 1) {
     warning("conf.level must be between (0,1). Setting to default.\n")
     conf.level <- 0.95
   }
+
 
   ##########################
   ## Binomial stats:
@@ -47,20 +63,22 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
   ##Other coefficient columns:
   ##CI.estimate, N, Nmiss2, depvar (show name of dependent variable), estimate, se, zstat
   ##Other  model fits:  logLik,AIC,BIC
-  binomial.stats.valid <- c(c("Nmiss","OR","CI.lower.OR","CI.upper.OR","p.value","concordance"), # default
-                 c("estimate","CI.OR","CI.estimate","CI.lower.estimate","CI.upper.estimate","N","Nmiss2","endpoint","std.error","statistic"),
-                 c("logLik","AIC","BIC","null.deviance","deviance","df.residual","df.null"))
+  binomial.stats.valid <- c(
+    "Nmiss", "OR", "CI.lower.OR", "CI.upper.OR", "p.value", "concordance",  # default
+    "estimate", "CI.OR", "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "N", "Nmiss2", "endpoint", "std.error", "statistic",
+    "logLik", "AIC", "BIC", "null.deviance", "deviance", "df.residual", "df.null"
+  )
 
-  if(any(!(binomial.stats %in% binomial.stats.valid))) {
+  if(any(binomial.stats %nin% binomial.stats.valid)) {
     stop(paste0("Invalid binomial stats: ",
-           paste(binomial.stats[!(binomial.stats %in% binomial.stats.valid)],collapse=","), "\n"))
+           paste(binomial.stats[binomial.stats %nin% binomial.stats.valid],collapse=","), "\n"))
   }
   ## let CI.OR decode to CI.lower.OR and CI.upper.OR
-  if(any(grepl("CI.OR", binomial.stats))) {
-    binomial.stats <- unique(c("CI.lower.OR","CI.upper.OR", binomial.stats[-grep("CI.OR",binomial.stats)]))
+  if(any(binomial.stats == "CI.OR")) {
+    binomial.stats <- unique(c(binomial.stats[binomial.stats != "CI.OR"], "CI.lower.OR", "CI.upper.OR"))
   }
-  if(any(grepl("CI.estimate", binomial.stats))) {
-    binomial.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", binomial.stats[-grep("CI.estimate",binomial.stats)]))
+  if(any(binomial.stats == "CI.estimate")) {
+    binomial.stats <- unique(c(binomial.stats[binomial.stats != "CI.estimate"], "CI.lower.estimate", "CI.upper.estimate"))
   }
 
   ##########################
@@ -68,40 +86,44 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
   ##########################
   ##Other coefficient columns: CI.estimate, N, Nmiss2, t.stat, standard.estimate, endpoint
   ##Other model fits: r.squared, AIC, BIC,logLik
-  gaussian.stats.valid <- c(c("Nmiss","estimate","std.error","p.value", "adj.r.squared"), #default
-              c("CI.estimate","CI.lower.estimate","CI.upper.estimate", "N", "Nmiss2", "statistic", "standard.estimate", "endpoint"),
-              c("r.squared", "AIC", "BIC","logLik","statistic.F","p.value.F"))
+  gaussian.stats.valid <- c(
+    "Nmiss", "estimate", "std.error", "p.value", "adj.r.squared",  #default
+    "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "N", "Nmiss2", "statistic", "standard.estimate", "endpoint",
+    "r.squared", "AIC", "BIC", "logLik", "statistic.F", "p.value.F"
+  )
 
-  if(any(!(gaussian.stats %in% gaussian.stats.valid))) {
+  if(any(gaussian.stats %nin% gaussian.stats.valid)) {
     stop(paste0("Invalid gaussian stats: ",
-           paste(gaussian.stats[!(gaussian.stats %in% gaussian.stats.valid)],collapse=","), "\n"))
+           paste(gaussian.stats[gaussian.stats %nin% gaussian.stats.valid],collapse=","), "\n"))
   }
-  if(any(grepl("CI.estimate", gaussian.stats))) {
-    gaussian.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", gaussian.stats[-grep("CI.estimate",gaussian.stats)]))
+  if(any(gaussian.stats == "CI.estimate")) {
+    gaussian.stats <- unique(c(gaussian.stats[gaussian.stats != "CI.estimate"], "CI.lower.estimate", "CI.upper.estimate"))
   }
 
 
-##########################
+  ##########################
   ## Poisson stats:
   ##########################
   ##(quasi)/poisson.stats=c("Nmiss","RR","CI.RR", "p.value","concordance"),
   ##Other coeff columns: CI.estimate, CI.RR  (ci for relrisk),N,Nmiss2, std.error, estimate, z.stat, endpoint
   ##Other model fits: AIC,BIC,logLik, dispersion
   ##  dispersion = deviance/df.residual
-  poisson.stats.valid <- c(c("RR","CI.lower.RR","CI.upper.RR", "p.value","concordance", "Nmiss"), # default
-                   c("CI.RR","CI.estimate","CI.lower.estimate","CI.upper.estimate", "CI.RR", "Nmiss2", "std.error", "estimate", "statistic","endpoint"),
-                   c("AIC", "BIC","logLik","dispersion","null.deviance","deviance","df.residual","df.null"))
+  poisson.stats.valid <- c(
+    "RR", "CI.lower.RR", "CI.upper.RR", "p.value", "concordance", "Nmiss", # default
+    "CI.RR", "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "CI.RR", "Nmiss2", "std.error", "estimate", "statistic", "endpoint",
+    "AIC", "BIC", "logLik", "dispersion", "null.deviance", "deviance", "df.residual", "df.null"
+  )
 
-  if(any(!(poisson.stats %in% poisson.stats.valid))) {
+  if(any(poisson.stats %nin% poisson.stats.valid)) {
       stop(paste0("Invalid poisson stats: ",
-           paste(poisson.stats[!(poisson.stats %in% poisson.stats.valid)],collapse=","), "\n"))
+           paste(poisson.stats[poisson.stats %nin% poisson.stats.valid],collapse=","), "\n"))
   }
    ## let CI.RR decode to CI.lower.RR and CI.upper.RR
-  if(any(grepl("CI.RR", poisson.stats))) {
-    poisson.stats <- unique(c("CI.lower.RR","CI.upper.RR", poisson.stats[-grep("CI.RR",poisson.stats)]))
+  if(any(poisson.stats == "CI.RR")) {
+    poisson.stats <- unique(c(poisson.stats[poisson.stats != "CI.RR"], "CI.lower.RR", "CI.upper.RR"))
   }
-  if(any(grepl("CI.estimate", poisson.stats))) {
-    poisson.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", poisson.stats[-grep("CI.estimate",poisson.stats)]))
+  if(any(poisson.stats == "CI.estimate")) {
+    poisson.stats <- unique(c(poisson.stats[poisson.stats == "CI.estimate"], "CI.lower.estimate", "CI.upper.estimate"))
   }
   ##########################
   ## Survival stats:
@@ -109,24 +131,27 @@ modelsum.control <- function(digits=3, nsmall=NULL, nsmall.ratio=2, digits.test=
   ##surv.stats=c(Nmiss,HR,CI.HR,p.value,concorance)
   ##Other possible coefficient table columns: CI.estimate,N,Nmiss2,estimate,se,endpoint,Nevents,z.stat
   ##Other possible model fits: r.squared, logLik, AIC, BIC
-  surv.stats.valid <- c(c("HR","CI.lower.HR","CI.upper.HR","p.value","concordance","Nmiss"), # default
-            c("CI.HR","CI.estimate","CI.lower.estimate","CI.upper.estimate","N","Nmiss2","estimate","std.error","endpoint","Nevents","statistic"),
-            c("r.squared", "logLik", "AIC", "BIC","statistic.sc","p.value.sc","p.value.log","p.value.wald","N","std.error.concordance"))
-  if(any(!(survival.stats %in% surv.stats.valid))) {
+  surv.stats.valid <- c(
+    "HR", "CI.lower.HR", "CI.upper.HR", "p.value", "concordance", "Nmiss", # default
+    "CI.HR", "CI.estimate", "CI.lower.estimate", "CI.upper.estimate", "N", "Nmiss2", "estimate", "std.error", "endpoint", "Nevents", "statistic",
+    "r.squared", "logLik", "AIC", "BIC", "statistic.sc", "p.value.sc", "p.value.log", "p.value.wald", "N", "std.error.concordance"
+  )
+
+  if(any(survival.stats %nin% surv.stats.valid)) {
     stop(paste0("Invalid survival stats: ",
-           paste(survival.stats[!(survival.stats %in% surv.stats.valid)],collapse=","), "\n"))
+           paste(survival.stats[survival.stats %nin% surv.stats.valid], collapse=","), "\n"))
   }
 
   ## let CI.HR decode to CI.lower.HR and CI.upper.HR
-  if(any(grepl("CI.HR", survival.stats))) {
-    survival.stats <- unique(c("CI.lower.HR","CI.upper.HR", survival.stats[-grep("CI.HR",survival.stats)]))
+  if(any(survival.stats == "CI.HR")) {
+    survival.stats <- unique(c(survival.stats[survival.stats != "CI.HR"], "CI.lower.HR", "CI.upper.HR"))
   }
-  if(any(grepl("CI.estimate", survival.stats))) {
-    survival.stats <- unique(c("CI.lower.estimate","CI.upper.estimate", survival.stats[-grep("CI.estimate",survival.stats)]))
+  if(any(survival.stats == "CI.estimate")) {
+    survival.stats <- unique(c(survival.stats[survival.stats != "CI.estimate"], "CI.lower.estimate", "CI.upper.estimate"))
   }
-  return(list(digits=digits, digits.test=digits.test, nsmall=nsmall, nsmall.ratio=nsmall.ratio,
+  return(list(digits=digits, digits.ratio=digits.ratio, digits.p = digits.p, format.p = format.p,
               show.adjust=show.adjust, show.intercept=show.intercept, conf.level=conf.level,
               binomial.stats=binomial.stats, gaussian.stats=gaussian.stats,
-              poisson.stats=poisson.stats, survival.stats=survival.stats))
+              poisson.stats=poisson.stats, survival.stats=survival.stats, stat.labels = stat.labels))
 
 }
