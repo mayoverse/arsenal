d3e7485d4b9fcc95dfc65ea08a312ca369ca085e
Ethan Heinzen <heinzen.ethan@mayo.edu>
2017-12-23 22:33:01 -0600

    Add test for non-syntactic names (#45)

--- tests/testthat/test_modelsum.R
+++ tests/testthat/test_modelsum.R
@@ -298,7 +298,7 @@ test_that("07/27/2017: modelsum labels (#13)", {
 
 #################################################################################################################################
 
-test_that("12/23/2017: non-syntactic column names trouble (#44)", {
+test_that("12/23/2017: non-syntactic names (#44, #45)", {
   dat <- data.frame(y = 1:10, x1x = rep(c("A", "B"), each = 5),
                     `1x` = rep(c("C", "D"), each = 5),
                     stringsAsFactors = FALSE, check.names = FALSE)
@@ -311,6 +311,16 @@ test_that("12/23/2017: non-syntactic column names trouble (#44)", {
       "|:-----------|:--------|:---------|:-------|:-------------|",
       "|(Intercept) |3.000    |0.707     |0.003   |0.727         |",
       "|**x1x B**   |5.000    |1.000     |0.001   |              |"
+    )
+  )
+  expect_identical(
+    capture.output(summary(modelsum(y ~ `1x`, data = dat))),
+    c(""                                                           ,
+      ""                                                           ,
+      "|            |estimate |std.error |p.value |adj.r.squared |",
+      "|:-----------|:--------|:---------|:-------|:-------------|",
+      "|(Intercept) |3.000    |0.707     |0.003   |0.727         |",
+      "|**1x D**    |5.000    |1.000     |0.001   |              |"
     )
   )
 })
