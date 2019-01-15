a740bc2b6308a6e4080acb144cdac4fd5f028b5a
Ethan Heinzen <heinzen.ethan@mayo.edu>
2019-01-15 13:55:45 -0600

    Update tests

--- tests/testthat/test_write2.R
+++ tests/testthat/test_write2.R
@@ -5,16 +5,18 @@ context("Testing the write2 output")
 
 data(mockstudy)
 
-expect_write2_worked <- function(FUN, object, reference, ...)
+expect_write2_worked <- function(FUN, object, reference, ..., render. = TRUE)
 {
   FUN <- match.fun(FUN)
   filename <- tempfile()
-  on.exit(expect_true(file.remove(paste0(filename, ".md"))))
+  on.exit(expect_true(file.remove(paste0(filename, ".Rmd"))))
+  if(render.) on.exit(expect_true(file.remove(filename)), add = TRUE)
   if(!file.exists(reference)) skip("Couldn't find the reference file.")
-  if(!file.create(paste0(filename, ".md"))) skip("Couldn't create the temporary file.")
+  if(!file.create(paste0(filename, ".Rmd"))) skip("Couldn't create the temporary file.")
   if(!grepl("arsenal/", getwd(), fixed = TRUE)) skip("These tests only run in Ethan's space.")
-  FUN(object, file = filename, ..., render. = FALSE, keep.md = TRUE, append. = FALSE)
-  generated <- readLines(paste0(filename, ".md"))
+
+  expect_error(FUN(object, file = filename, ..., render. = render., keep.rmd = TRUE, append. = FALSE, quiet = TRUE), NA)
+  generated <- readLines(paste0(filename, ".Rmd"))
   expect_output_file(cat(generated, sep = "\n"), reference)
 }
 
@@ -24,13 +26,13 @@ expect_write2_worked <- function(FUN, object, reference, ...)
 
 test_that("write2.tableby -> HTML", {
   expect_write2_worked(write2html, tableby(arm ~ sex + age, data=mockstudy, numeric.stats = c("meansd", "q1q3", "range")),
-                       reference = "write2.tableby.html.md",
+                       reference = "write2.tableby.html.Rmd",
                        title = "My test table", labelTranslations = list(sex = "SEX", age ="Age, yrs"), total = FALSE)
 })
 
 test_that("write2.modelsum -> HTML", {
   expect_write2_worked(write2html, modelsum(alk.phos ~ arm + ps + hgb, adjust= ~ age + sex, family = "gaussian", data = mockstudy),
-                       reference = "write2.modelsum.html.md",
+                       reference = "write2.modelsum.html.Rmd",
                        title = "My test table", show.intercept = FALSE, digits = 5)
 })
 
@@ -38,12 +40,12 @@ old.labs <- c(cumFreq = "cumFreq", freqPercent = "freqPercent", cumPercent = "cu
 
 test_that("write2.freqlist -> HTML", {
   expect_write2_worked(write2html, freqlist(table(mockstudy[c("arm", "sex", "mdquality.s")], useNA = "ifany"), strata = c("arm", "sex")),
-                       reference = "write2.freqlist.html.md", single = TRUE, labelTranslations = old.labs)
+                       reference = "write2.freqlist.html.Rmd", single = TRUE, labelTranslations = old.labs)
 })
 
 test_that("write2.freqlist -> doc", {
   expect_write2_worked(write2word, freqlist(table(mockstudy[c("arm", "sex", "mdquality.s")], useNA = "ifany"), strata = c("arm", "sex")),
-                       reference = "write2.freqlist.doc.md", single = TRUE, title = "My cool title", labelTranslations = old.labs)
+                       reference = "write2.freqlist.doc.Rmd", single = TRUE, title = "My cool title", labelTranslations = old.labs)
 })
 
 ## From the vignette
@@ -54,7 +56,7 @@ test_that("write2.list (summary objects) -> PDF", {
     summary(modelsum(age ~ sex, data = mockstudy), title = "A Title for modelsum"),
     summary(freqlist(~ sex, data = mockstudy, labelTranslations = old.labs), title = "A Title for freqlist")
   )
-  expect_write2_worked(write2pdf, mylist6, reference = "write2.multititles.pdf.md")
+  expect_write2_worked(write2pdf, mylist6, reference = "write2.multititles.pdf.Rmd")
 })
 
 ###########################################################################################################
@@ -64,14 +66,14 @@ test_that("write2.list (summary objects) -> PDF", {
 # test_that("write2.knitr_kable -> HTML", {
 #   if(require(knitr))
 #   {
-#     expect_write2_worked(write2html, knitr::kable(head(mockstudy)), reference = "write2.kable.html.md")
+#     expect_write2_worked(write2html, knitr::kable(head(mockstudy)), reference = "write2.kable.html.Rmd")
 #   } else skip("library(knitr) not available.")
 # })
 #
 # test_that("write2.xtable -> HTML", {
 #   if(require(xtable))
 #   {
-#     expect_write2_worked(write2html, xtable::xtable(head(mockstudy), caption = "My xtable"), reference = "write2.xtable.html.md",
+#     expect_write2_worked(write2html, xtable::xtable(head(mockstudy), caption = "My xtable"), reference = "write2.xtable.html.Rmd",
 #                          type = "html", comment = FALSE, include.rownames = FALSE, caption.placement = 'top')
 #   } else skip("library(xtable) not available.")
 # })
@@ -79,7 +81,7 @@ test_that("write2.list (summary objects) -> PDF", {
 # test_that("write2.character (pander) -> HTML", {
 #   if(require(pander))
 #   {
-#     expect_write2_worked(write2html, pander::pander_return(head(mockstudy)), reference = "write2.pander.html.md")
+#     expect_write2_worked(write2html, pander::pander_return(head(mockstudy)), reference = "write2.pander.html.Rmd")
 #   } else skip("library(pander) not available.")
 # })
 #
@@ -96,16 +98,16 @@ mylist2 <- list("# Header 1",
                 tableby(sex ~ age, data = mockstudy, numeric.stats = c("meansd", "q1q3", "range")))
 
 test_that("write2.list -> PDF", {
-  expect_write2_worked(write2pdf, mylist, reference = "write2.mylist.pdf.md")
+  expect_write2_worked(write2pdf, mylist, reference = "write2.mylist.pdf.Rmd")
 })
 
 test_that("write2.list -> Word", {
-  expect_write2_worked(write2word, mylist2, reference = "write2.mylist2.doc.md")
+  expect_write2_worked(write2word, mylist2, reference = "write2.mylist2.doc.Rmd")
 })
 
 test_that("write2.list recursion -> PDF", {
   expect_write2_worked(write2word, list(mylist2, mylist),
-                       reference = "write2.mylists.pdf.md")
+                       reference = "write2.mylists.pdf.Rmd")
 })
 
 
@@ -116,14 +118,23 @@ test_that("write2.list recursion -> PDF", {
 my.lm <- summary(lm(age ~ sex, data = mockstudy))
 test_that("write2.default -> PDF", {
   expect_write2_worked(write2pdf, my.lm,
-                       reference = "write2.lm.pdf.md")
+                       reference = "write2.lm.pdf.Rmd")
 })
 
 test_that("write2.verbatim -> html", {
   expect_write2_worked(write2pdf, verbatim(paste0("Hi.", 1:5)),
-                       reference = "write2.char.html.md")
+                       reference = "write2.char.html.Rmd")
 })
 
+
+test_that("Writing HTML from PDF works (#162)", {
+  expect_write2_worked(write2pdf, list(
+    "hi there",
+    code.chunk(
+      arsenal::write2html("hi there", "hi_there.html", clean = TRUE)
+    )
+  ), reference = "write2.render.html.Rmd")
+})
 
 ###########################################################################################################
 #### YAML output
@@ -136,7 +147,8 @@ mylist3 <- list(
   yaml(title = "My title"),
   my.lm,
   yaml(author = "Ethan P Heinzen"),
-  yaml("header-includes" = list("\\usepackage[labelformat=empty]{caption}"))
+  yaml("header-includes" = list("\\usepackage[labelformat=empty]{caption}")),
+  code.chunk(a <- 1, "b <- 2", a + b, "a - b", chunk.opts = "r echo = FALSE, eval = TRUE")
 )
 
 mylist4 <- list(
@@ -144,12 +156,13 @@ mylist4 <- list(
   "# Header 1",
   "This is a small paragraph.",
   tableby(sex ~ age, data = mockstudy, numeric.stats = c("meansd", "q1q3", "range")),
-  my.lm
+  my.lm,
+  code.chunk(a <- 1, "b <- 2", a + b, "a - b", chunk.opts = "r echo = FALSE, eval = TRUE")
 )
 
 test_that("write2.yaml -> PDF", {
-  expect_write2_worked(write2pdf, mylist3, reference = "write2.yaml.pdf.md")
-  expect_write2_worked(write2pdf, mylist4, reference = "write2.yaml.pdf.md")
+  expect_write2_worked(write2pdf, mylist3, reference = "write2.yaml.pdf.Rmd")
+  expect_write2_worked(write2pdf, mylist4, reference = "write2.yaml.pdf.Rmd")
 })
 
 
