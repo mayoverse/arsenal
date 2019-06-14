479f7e428853cbc8a6fe70dab94fa238dd346d50
Ethan Heinzen <heinzen.ethan@mayo.edu>
2019-06-14 11:34:22 -0500

    Remove testthat::compare. See #179

--- R/defunct.R
+++ R/defunct.R
@@ -3,14 +3,35 @@
 #'
 #' Details about defunct functions in \code{arsenal}
 #'
+#' @param x,y See \code{\link{comparedf}}.
+#' @param ... Other arguments.
 #' @details
+#' \code{comparison.control} was renamed to \code{\link{comparedf.control}} in version 3.0.0.
+#'
+#' \code{compare.data.frame} was renamed to \code{\link{comparedf}} in version 3.0.0.
+#'
 #' \code{length.tableby} was removed in version 2.0.0.
 #'
 #' \code{includeNA.character} and \code{includeNA.numeric} were removed in version 2.0.0 and
 #'   replaced with a default method.
 #'
 #' \code{rangeTime} was removed in version 1.5.0.
-#' @seealso \code{\link{arsenal-deprecated}}
+#' @seealso \code{\link{arsenal-deprecated}}, \code{\link{comparedf}}
 #' @name arsenal-defunct
 NULL
 #> NULL
+
+
+#' @rdname arsenal-defunct
+#' @export
+comparison.control <- function(...)
+{
+  .Defunct("comparedf.control", package = "arsenal")
+}
+
+#' @rdname arsenal-defunct
+#' @export
+compare.data.frame <- function(x, y, ...)
+{
+  .Defunct("comparedf", package = "arsenal")
+}
