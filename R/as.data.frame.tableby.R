2406e225465126456451be067098aeeeb9887bfd
Ethan Heinzen <heinzen.ethan@mayo.edu>
2020-10-26 13:29:05 -0500

    can't use isFALSE because 3.4.x... cc #288

--- R/as.data.frame.tableby.R
+++ R/as.data.frame.tableby.R
@@ -84,19 +84,19 @@ as_data_frame_tableby <- function(byList, control)
   simp.ord <- lapply(byList$control.list, f, "ordered.simplify")
   simp.dat <- vapply(byList$control.list, f, NA, "date.simplify")
 
-  if(!all(vapply(simp.cat, isFALSE, NA)) || any(simp.num) || !all(vapply(simp.ord, isFALSE, NA)) || any(simp.dat))
+  if(!all(vapply(simp.cat, identical, NA, FALSE)) || any(simp.num) || !all(vapply(simp.ord, identical, NA, FALSE)) || any(simp.dat))
   {
     simplify <- function(x)
     {
       ## make sure there's only two lines of (the same) summary statistic, and that it's categorical
-      if(!isFALSE(simp.cat[[x$variable[1]]]) && all(x$variable.type == "categorical") &&
+      if(!identical(simp.cat[[x$variable[1]]], FALSE) && all(x$variable.type == "categorical") &&
          (nrow(x) == 2 || nrow(x) == 3 && x$term[2] == x$term[3]))
       {
         y <- x[nrow(x), , drop = FALSE]
         y$term[1] <- x$term[1]
         y$label[1] <- paste0(x$label[1], if(identical(simp.cat[[x$variable[1]]], "label")) paste0(" (", y$label[1], ")"))
 
-      } else if(!isFALSE(simp.ord[[x$variable[1]]]) && all(x$variable.type == "ordinal") &&
+      } else if(!identical(simp.ord[[x$variable[1]]], FALSE) && all(x$variable.type == "ordinal") &&
                 (nrow(x) == 2 || nrow(x) == 3 && x$term[2] == x$term[3]))
       {
         y <- x[nrow(x), , drop = FALSE]
