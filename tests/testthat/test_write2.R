63ae450cade9208216814e22092735332616a703
Ethan Heinzen <heinzen.ethan@mayo.edu>
2021-02-15 11:59:33 -0600

    skip write2 on mac

--- tests/testthat/test_write2.R
+++ tests/testthat/test_write2.R
@@ -8,6 +8,7 @@ data(mockstudy)
 expect_write2_worked <- function(FUN, object, reference, ...)
 {
   skip_on_cran()
+  skip_on_os("mac")
   FUN <- match.fun(FUN)
   filename <- tempfile()
   # on.exit(expect_true(file.remove(paste0(filename, ".Rmd"))))
